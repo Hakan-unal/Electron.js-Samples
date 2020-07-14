be8ecb8a0360a6dda8d76ba13c2db56bf9d41d09
Hakan-unal <hakanunal_1996@hotmail.com>
2020-07-14 16:24:31 +0300

    updated

--- sample 3/my-application/src/index.js
+++ sample 3/my-application/src/index.js
@@ -25,6 +25,14 @@ const createWindow = () => {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
+// Require objesinin çalışması için gerekli
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+      webPreferences: {
+          nodeIntegration: true
+      }
+  });
+});
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
