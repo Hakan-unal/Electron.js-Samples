7961afd9dab4bc8c5263c4555d634fdc227e497a
Hakan-unal <hakanunal_1996@hotmail.com>
2020-07-16 09:06:14 +0300

    updated

--- sample 3/my-application/src/scripts/location.js
+++ sample 3/my-application/src/scripts/location.js
@@ -5,17 +5,28 @@ const clearButton = document.querySelector("#clearButton");
 
 const electron = require("electron");
 const { ipcRenderer } = electron;
-let listArray;
+let listArray = [];
+
+const itemAddToList = (item) => {
+    listArray.push(item);
+    let li = document.createElement("li");
+    li.setAttribute("class", "list-group-item");
+    li.innerText = item;
+    list.appendChild(li);
+}
+
+const clearList = () => {
+    list.innerHTML = "";
+    listArray = [];
+}
+
 
 button.addEventListener("click", (event) => {
     event.preventDefault();
     console.log(inputBox.value)
     ipcRenderer.send("button:click", inputBox.value);
+    itemAddToList(inputBox.value);
     inputBox.value = "";
 })
 
-
-
-localStorage.setItem("test", JSON.stringify(array));
-
-console.log(JSON.parse(localStorage.getItem("test")));
+clearButton.addEventListener("click", clearList);
