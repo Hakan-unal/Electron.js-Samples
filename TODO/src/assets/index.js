c3f1a3dc7f59249eba7d1a6c6706bdd715476b1b
Hakan-unal <hakanunal_1996@hotmail.com>
2020-07-27 10:15:07 +0300

    updated

---
+++ TODO/src/assets/index.js
@@ -0,0 +1,7 @@
+const electron = require("electron")
+const { ipcRenderer } = electron;
+
+const form = document.querySelector("#form");
+const inputBox = document.querySelector("#inputBox");
+const todoList=document.querySelector("#todoList")
+
